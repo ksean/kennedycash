1990a8dc36fc88e405a3ec3d8b4726a99d194d61
Sean Kennedy <sean@kennedy.software>
2018-03-31 11:36:43 -0400

    Added Category class, updated test to pass again

--- backend/kennedycash/models.py
+++ backend/kennedycash/models.py
@@ -2,7 +2,24 @@ from django.db import models
 from djmoney.models.fields import MoneyField
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+
 class PersonalTransaction(models.Model):
     date = models.DateField()
     description = models.TextField()
     amount = MoneyField(max_digits=10, decimal_places=2, default_currency='CAD')
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return "%s | %s [%s] | %s" % (self.date, self.description, self.amount, self.categories)
+
+    class Meta:
+        ordering = ('date','description','amount',)
--- backend/kennedycash/tests.py
+++ backend/kennedycash/tests.py
@@ -1,14 +1,24 @@
+from datetime import datetime
 from django.test import TestCase
-from .models import PersonalTransaction
+from kennedycash.models import *
+from djmoney.money import Money
 
 
-class PersonalTransactionModelTest(TestCase):
+class PersonalTransactionTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        PersonalTransaction.objects.create(description='a description here')
+        # Run once before all tests
+        Category.objects.create(name='foo')
+        category = Category.objects.get(id=1)
+        PersonalTransaction.objects.create(date=datetime.now().date(),
+                                           description='a description here',
+                                           amount=Money(10, 'CAD'))
+        personalTransaction = PersonalTransaction.objects.get(id=1)
+        personalTransaction.categories.add(category)
+
 
     def test_description_content(self):
-        personalTransaction = PersonalTransaction.objects.get(id=2)
-        expected_object_name = f'{personalTransaction.description}'
+        personalTransaction = PersonalTransaction.objects.get(id=1)
+        expected_object_name = '%s' % (personalTransaction.description,)
         self.assertEquals(expected_object_name, 'a description here')
